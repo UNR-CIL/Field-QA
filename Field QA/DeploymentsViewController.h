d25719d3c609aff729cc4b3aa738c00f3230e20f
John Jusayan <john@renocollective.com>
2014-04-16 12:03:04 -0700

    Added view controllers for deployments

---
+++ Field QA/DeploymentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeploymentsViewController.h
+//  Field QA
+//
+//  Created by John Jusayan on 4/16/14.
+//  Copyright (c) 2014 CSE UNR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeploymentsViewController : UITableViewController
+
+@end
