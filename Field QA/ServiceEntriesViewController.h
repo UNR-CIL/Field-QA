b11a4d91423971ae2a6274fb6d70867ebb7e4ed5
John Jusayan <john@renocollective.com>
2014-04-16 12:17:10 -0700

    Added view controllers for Service Entries

---
+++ Field QA/ServiceEntriesViewController.h
@@ -0,0 +1,13 @@
+//
+//  ServiceEntriesViewController.h
+//  Field QA
+//
+//  Created by John Jusayan on 4/16/14.
+//  Copyright (c) 2014 CSE UNR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ServiceEntriesViewController : UITableViewController
+
+@end
