65a47e7037bd15764991c8b87e5b0885357811fd
John Jusayan <john@renocollective.com>
2014-06-24 15:34:53 -0700

    Initial setup for appledoc

--- Field QA/Person.h
+++ Field QA/Person.h
@@ -13,34 +13,99 @@
 
 @interface Person : NSManagedObject
 
+/**Contact email
+ */
 @property (nonatomic, retain) NSNumber * email;
+
+/**First name
+ */
 @property (nonatomic, retain) NSString * firstName;
+
+/**Last name
+ */
 @property (nonatomic, retain) NSString * lastName;
+
+/**Phone number
+ */
 @property (nonatomic, retain) NSString * phone;
+
+/**Associated components
+ */
 @property (nonatomic, retain) NSSet *components;
+
+/**Deployments created by this person
+ */
 @property (nonatomic, retain) NSSet *createdDeployments;
+
+/**Deployments managed by this person
+ */
 @property (nonatomic, retain) NSSet *managedDeployments;
+
+/**Projects owned by this person
+ */
 @property (nonatomic, retain) NSSet *projects;
+
+/**Service entries associated with this person
+ */
 @property (nonatomic, retain) NSSet *serviceEntries;
+
+/**Service entries created by this person
+ */
 @property (nonatomic, retain) NSSet *serviceEntriesCreated;
+
+/**Systems associated with this person
+ */
 @property (nonatomic, retain) NSSet *systems;
 @end
 
 @interface Person (CoreDataGeneratedAccessors)
 
+/**Add a component
+ */
 - (void)addComponentsObject:(Component *)value;
+
+/**Remove a component
+ */
 - (void)removeComponentsObject:(Component *)value;
+
+/**Add several components
+ */
 - (void)addComponents:(NSSet *)values;
+
+/**Remove several components
+ */
 - (void)removeComponents:(NSSet *)values;
 
+/**Add a deployment created by this person
+ */
 - (void)addCreatedDeploymentsObject:(Deployment *)value;
+
+/**Remove a deployment created by thsi person
+ */
 - (void)removeCreatedDeploymentsObject:(Deployment *)value;
+
+/**Add several deployments created by this user
+ */
 - (void)addCreatedDeployments:(NSSet *)values;
+
+/**Remove several deployments created by this user
+ */
 - (void)removeCreatedDeployments:(NSSet *)values;
 
+/**Add a deployment managed by this person
+ */
 - (void)addManagedDeploymentsObject:(Deployment *)value;
+
+/**Remove a deployment managed by this person
+ */
 - (void)removeManagedDeploymentsObject:(Deployment *)value;
+
+/**Add several deployments managed by this person
+ */
 - (void)addManagedDeployments:(NSSet *)values;
+
+/**Remove several deployments managed by this person
+ */
 - (void)removeManagedDeployments:(NSSet *)values;
 
 - (void)addProjectsObject:(Project *)value;
--- Field QA/ServiceEntry.h
+++ Field QA/ServiceEntry.h
@@ -13,38 +13,111 @@
 
 @interface ServiceEntry : NSManagedObject
 
+/**Category type description
+ */
 @property (nonatomic, retain) NSString * category;
+
+/**Date this service entry was created
+ */
 @property (nonatomic, retain) NSDate * creationDate;
+
+/**Date this service entry was most recently modified
+ */
 @property (nonatomic, retain) NSDate * modificationDate;
+
+/**Notes for this entry
+ */
 @property (nonatomic, retain) NSString * notes;
+
+/**Thumbnail photo
+ */
 @property (nonatomic, retain) id photo;
+
+/**Associated components
+ */
 @property (nonatomic, retain) NSSet *components;
+
+/**Person who created this entry
+ */
 @property (nonatomic, retain) Person *creator;
+
+/**Associated deployments
+ */
 @property (nonatomic, retain) NSSet *deployments;
+
+/**Associated systems
+ */
 @property (nonatomic, retain) NSSet *systems;
+
+/**Associated users
+ */
 @property (nonatomic, retain) NSSet *users;
 @end
 
 @interface ServiceEntry (CoreDataGeneratedAccessors)
 
+/**Add a component
+ */
 - (void)addComponentsObject:(Component *)value;
+
+/**Remove a component
+ */
 - (void)removeComponentsObject:(Component *)value;
+
+/**Add several components
+ */
 - (void)addComponents:(NSSet *)values;
+
+/**Remove several components
+ */
 - (void)removeComponents:(NSSet *)values;
 
+/**Add a deployment
+ */
 - (void)addDeploymentsObject:(Deployment *)value;
+
+/**Remove a deployment
+ */
 - (void)removeDeploymentsObject:(Deployment *)value;
+
+/**Add several deployments
+ */
 - (void)addDeployments:(NSSet *)values;
+
+/**Remove several deployments
+ */
 - (void)removeDeployments:(NSSet *)values;
 
+/**Add a system
+ */
 - (void)addSystemsObject:(System *)value;
+
+/**Remove a system
+ */
 - (void)removeSystemsObject:(System *)value;
+
+/**Add several systems
+ */
 - (void)addSystems:(NSSet *)values;
+
+/**Remove several systems
+ */
 - (void)removeSystems:(NSSet *)values;
 
+/**Add a user
+ */
 - (void)addUsersObject:(Person *)value;
+
+/**Remove a user
+ */
 - (void)removeUsersObject:(Person *)value;
+
+/**Add several users
+ */
 - (void)addUsers:(NSSet *)values;
+
+/**Remove several users
+ */
 - (void)removeUsers:(NSSet *)values;
 
 @end
